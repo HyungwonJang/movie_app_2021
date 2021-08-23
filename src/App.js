a023315e92fc9ff677ed493aca6c0363bab0af5b
jhw4416 <77849146+jhw4416-walpurgis@users.noreply.github.com>
2021-08-24 00:48:06 +0900

    0824

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types'
+
+const foodILike = [
+  {
+    id:1,
+    name: "kimchi",
+    image:"https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png"
+  },
+  {
+    id:2,
+    name: "bibimbap",
+    image:"https://recipe1.ezmember.co.kr/cache/recipe/2018/10/03/355b5cd5c3beb1a775c82ee425dcd1931.jpg"
+  },
+  {
+    id:3,
+    name: "samgyeopsal",
+    image:"https://post-phinf.pstatic.net/MjAyMDAzMDNfMTcg/MDAxNTgzMTkwNjA3ODQ5.kUXPHqGJ2xPDSu_3FiEoFC3kY9QyQ_g9CziCGrFSDuEg.LpCfOTbc5qth9d-GKzGv9jwj2VKhcqmPHp5cp1KJYEsg.JPEG/IM_food02.jpg"
+  }
+]
+
+function Food({name, ficture, rating}) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
+      <img src={ficture} alt={name} />
+    </div>
+  )
+}
+
+Food.propTypes={
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map(dish => <Food key={dish.id} name={dish.name} ficture={dish.image} rating={dish.rating} />)}
     </div>
   );
 }
